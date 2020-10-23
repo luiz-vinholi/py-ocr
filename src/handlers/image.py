a50f3c43ec532e32868857396022a2da4628145f
luiz-vinholi <luizpaivavinholi@gmail.com>
2020-10-22 23:05:14 -0300

    Image handler abstract in a class

---
+++ src/handlers/image.py
@@ -0,0 +1,34 @@
+import PIL as pillow
+import numpy as np
+import cv2
+
+
+class Image:
+    def __init__(self, image_src: str):
+        image = pillow.Image.open(image_src).convert('RGB')
+        image = np.asarray(image).astype(np.uint8)
+
+        self._this_image = image
+        self.treated_image = None
+
+    @property
+    def this_image(self):
+        return pillow.Image.fromarray(self._this_image)
+
+    def treat(self):
+        image = cv2.detailEnhance(self._this_image, sigma_s=100, sigma_r=1)
+
+        image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)[1]
+
+        treated_image = pillow.Image.fromarray(thresh)
+        treated_image.save('./src/images/treated-cpfl.jpeg')
+
+        self.treated_image = treated_image
+
+        return self
+
+    def crop(self, areas: tuple):
+        cropped_image = self._this_image.crop(areas)
+        cropped_image.save('./src/images/cropped-cpfl.jpeg')
+        return cropped_image
--- src/main.py
+++ src/main.py
@@ -1,68 +1,50 @@
 import pytesseract as tesseract
-import numpy
-import cv2
 
-from PIL import Image
 from pytesseract import Output
-from typing import Any, Tuple
 
-
-def treat_image():
-  image = Image.open('./src/images/cpfl.jpeg').convert('RGB')
-  image = numpy.asarray(image).astype(numpy.uint8)
-  image = cv2.detailEnhance(image, sigma_s=100, sigma_r=1)
-
-  image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-  thresh = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)[1]
-  
-  treated_image = Image.fromarray(thresh)
-  treated_image.save('./src/images/treated-cpfl.jpeg')
-
-  image = Image.fromarray(image)
-
-  return image, treated_image
+from handlers.image import Image
 
 
 def main():
-  original_image, treated_image = treat_image()
+    image = Image('./src/images/cpfl.jpeg').treat()
 
-  data = tesseract.image_to_data(
-    treated_image,
-    lang='por',
-    output_type=Output.DICT,
-  )
-
-  texts_data = [
-    {
-      'top': data['top'][index],
-      'left': data['left'][index],
-      'width': data['width'][index],
-      'height': data['height'][index],
-      'confidence': data['conf'][index],
-      'text': data['text'][index],
-    } for index, text in enumerate(data['text'])
-      if text.strip() and text == 'TOTAL'
-  ]
-
-  for text_data in texts_data:
-    areas = (
-      text_data['left'],
-      text_data['top'],
-      text_data['left'] + text_data['width'] + 70,
-      text_data['top'] + text_data['height'] + 20,
-    )
-
-    cropped_image = original_image.crop(areas)
-    cropped_image.save('./src/images/')
     data = tesseract.image_to_data(
-      cropped_image,
-      lang='por',
-      output_type=Output.DICT,
+        image.treated_image,
+        lang='por',
+        output_type=Output.DICT,
     )
-    print(data)
-    
-  print(texts_data)
-  
+
+    texts_data = [
+        {
+            'top': data['top'][index],
+            'left': data['left'][index],
+            'width': data['width'][index],
+            'height': data['height'][index],
+            'confidence': data['conf'][index],
+            'text': data['text'][index],
+        } for index, text in enumerate(data['text'])
+        if text.strip() and text == 'TOTAL'
+    ]
+
+    for text_data in texts_data:
+        areas = (
+            text_data['left'],
+            text_data['top'],
+            text_data['left'] + text_data['width'],
+            text_data['top'] + text_data['height'] + 20,
+        )
+
+        cropped_image = image.this_image.crop(areas)
+        cropped_image.save('./src/images/cropped-cpfl.jpeg')
+        data = tesseract.image_to_data(
+            cropped_image,
+            lang='por',
+            output_type=Output.DICT,
+        )
+        print(data)
+
+    print(texts_data)
+
 
 if __name__ == '__main__':
-  main()
+    main()
